cc6f7833903df20194cfaec0cd1e21d7dbad91bb
adrianohpmaciel <adrianomacieldev@gmail.com>
2024-09-15 20:10:35 -0300

    Add 'Preparação para HTML e CSS' + 'Desenvolvimento 1'

---
+++ Talento Cloud - Proz Educação/Preparação para HTML e CSS/Desenvolvimento 1 - Arrays.py
@@ -0,0 +1,15 @@
+'''
+Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o comando print(). 
+O primeiro array deve conter os produtos de uma loja da sua escolha (loja de comida, materiais de construção, música, etc). 
+O segundo array deve conter os anos de nascimento de familiares e amigos seus. 
+Lembre-se de usar nomes descritivos para nomear cada variável, e de usar o tipo de dado apropriado para cada lista (strings, booleanos, números inteiros, floats).
+'''
+# Array de produtos de uma loja de materiais de construção
+produtos_loja_construcao = ["Cimento", "Tijolo", "Areia", "Ferragem", "Madeira"]
+
+# Array de anos de nascimento de amigos e familiares
+anos_nascimento_amigos_familia = [1991, 1988, 1995, 2002, 1983]
+
+# Imprimindo os arrays no terminal
+print("Produtos da loja de construção:", produtos_loja_construcao)
+print("Anos de nascimento de amigos e familiares:", anos_nascimento_amigos_familia)
